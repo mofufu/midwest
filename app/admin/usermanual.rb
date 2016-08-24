c5e3ff500114679fb34d8dfba47ea72c9d936e68
Prelang Builder <builder@prelang.com>
2016-08-24 18:19:24 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Usermanual' to allow for create, update

--- app/admin/usermanual.rb
+++ app/admin/usermanual.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Usermanual do
+  permit_params :document_name, :document_type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
